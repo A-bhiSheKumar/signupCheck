cf144777ac2f008065dc580a00f203d16ea2a078
A-bhiSheKumar <abhikumar8649@gmail.com>
2023-06-28 13:49:29 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# signupPage
+Hey there i am deploying my nodejs project its just a simple signup page but i am in a learning phase and i am going to make it live,,
---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const { STATUS_CODES } = require("http");
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+
+app.post("/",function(req,res){
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.emailId;
+   
+    const data ={
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields:{
+                    FNAME:firstName,
+                    LNAME:lastName
+                }
+            }
+        ]
+    };
+    const jsonData =JSON.stringify(data);
+    const url = "https://us21.api.mailchimp.com/3.0/lists/03e66331ea";
+
+    const option = {
+        method:"POST",
+        auth:"avi86:14d67361f316643e4f17e56cd79bd60f-us21"
+    }
+    const request = https.request(url,option,function(response){
+        response.on("data",function(data){
+            console.log(JSON.parse(data));
+        })
+        if (response.statusCode===200){
+            res.sendFile(__dirname + "/success.html");
+        }
+        else{
+            res.sendFile(__dirname + "/failure.html");
+        }
+    })
+    request.write(jsonData);
+    request.end();
+});
+
+
+// redirecting to the home page using failure page route
+
+app.post("/failure",function(req , res){
+    res.redirect("/");
+})
+
+app.post("/success" , function(req , res){
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("Server is ready to go....");
+})
+
+
+//Api key
+
+//14d67361f316643e4f17e56cd79bd60f-us21
+
+//list id
+
+//03e66331ea
